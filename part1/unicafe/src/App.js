2ebd1656d7fb769a4ac890360fec150aa4e011fe
LOKMITHAR S D <lokmitharlsd@gmail.com>
2023-03-02 14:36:15 +0530

    commit

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,17 +1,19 @@
 const App = () => {
     const course = 'Half Stack application development'
-    const part1 = {
-      name: 'Fundamentals of React',
-      exercises: 10
-    }
-    const part2 = {
-      name: 'Using props to pass data',
-      exercises: 7
-    }
-    const part3 = {
-      name: 'State of a component',
-      exercises: 14
-    }
+    const parts = [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
     const Header = (props) => {
         console.log(props)
         return(
@@ -35,9 +37,9 @@ const App = () => {
     const Content = () => {
         return(
             <div>
-                <Part part={part1.name} exer={part1.exercises}/>
-                <Part part={part2.name} exer={part2.exercises}/>
-                <Part part={part3.name} exer={part3.exercises}/>
+                <Part part={parts[0].name} exer={parts[0].exercises}/>
+                <Part part={parts[1].name} exer={parts[1].exercises}/>
+                <Part part={parts[2].name} exer={parts[2].exercises}/>
             </div>
         )
     }
@@ -45,7 +47,7 @@ const App = () => {
         return(
             <div>
                 <p>
-                    Number of Exercises {part1.exercises+part2.exercises+part3.exercises}
+                    Number of Exercises {parts[0].exercises+parts[1].exercises+parts[2].exercises}
                 </p>
             </div>
         )
