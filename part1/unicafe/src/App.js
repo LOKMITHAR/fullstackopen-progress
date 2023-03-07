77e0695db1a3d95d5d1cd21a1808062e922ec879
LOKMITHAR S D <lokmitharlsd@gmail.com>
2023-03-07 09:24:39 +0530

    commit

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,27 +1,16 @@
-const Hello = (props) => {
-    const name = props.name
-    const age = props.age
-  
-    const bornYear = () => new Date().getFullYear() - age
-  
-    return (
-      <div>
-        <p>Hello {name}, you are {age} years old</p>
-        <p>So you were probably born in {bornYear()}</p>
-      </div>
-    )
-  }
-  
-  const App = () => {
-    const name = 'Peter'
-    const age = 10
-  
-    return (
-      <div>
-        <h1>Greetings</h1>
-        <Hello name="Sam" age={21} />
-        <Hello name={name} age={age} />
-      </div>
-    )
-  }
-  export default App
+import { useState } from 'react'
+
+const App = () => {
+  const [ counter, setCounter ] = useState(0)
+
+  setTimeout(
+    () => setCounter(counter + 1),
+    1000
+  )
+
+  return (
+    <div>{counter}</div>
+  )
+}
+
+export default App
